0aff190f870c594f6a3cbd411c670f6a7280b542
Denis Veselov <veselov@trilan.ru>
2014-11-25 12:50:28 +0300

    add max recipients count settings for fast add method

--- banzai/settings.py
+++ banzai/settings.py
@@ -4,3 +4,5 @@ from django.conf import settings
 BANZAI_API_DOMAIN = getattr(settings, 'BANZAI_API_DOMAIN', 'api.get-n-post.ru')
 BANZAI_API_VERSION = getattr(settings, 'BANZAI_API_VERSION', 'v1')
 BANZAI_API_KEY = getattr(settings, 'BANZAI_API_KEY', '')
+BANZAI_API_FAST_ADD_METHOD_RECIPIENTS_MAX_COUNT = getattr(
+    settings, 'BANZAI_API_FAST_ADD_METHOD_RECIPIENTS_MAX_COUNT', 500)
