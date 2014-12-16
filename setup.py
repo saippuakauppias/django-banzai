db156ed47c6e68737534f4c2f357514277dc8386
Denis Veselov <progr-m@yandex.ru>
2014-12-16 13:47:11 +0300

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setup(
     name='django-banzai',
-    version='0.1',
+    version='0.2',
     license='MIT',
     description='Django API wrapper for mailing delivery '
                 'service: http://get-n-post.ru/',
